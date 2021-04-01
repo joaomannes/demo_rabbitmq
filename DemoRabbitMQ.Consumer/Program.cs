ec5970aba05d1d4e12da3e5ae015ec5afcb40f9d
João Gabriel Mannes <joaogm@joaogm.com.br>
2021-04-01 00:04:32 -0300

    Add project files.

---
+++ DemoRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,68 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace DemoRabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consume();
+        }
+
+        static void Consume()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "demo_rabbitmq",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    string message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine(" [x] Received {0}", message);
+
+
+                    Console.WriteLine("Ack {0}", message);
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    //if (ea.Redelivered)
+                    //{
+                    //    Console.WriteLine("Redelivery Ack {0}", message);
+                    //    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    //}
+                    //else
+                    //{
+                    //    if (Int32.Parse(message) % 2 == 0)
+                    //    {
+                    //        Console.WriteLine("Ack {0}", message);
+                    //        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    //    }
+                    //    else
+                    //    {
+                    //        Console.WriteLine("Reject Ack");
+                    //        channel.BasicReject(deliveryTag: ea.DeliveryTag, true);
+                    //    }
+                    //}
+
+
+                };
+                channel.BasicConsume(queue: "demo_rabbitmq",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine("Aguardando Mensagens...");
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ DemoRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace DemoRabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                int opcao = 0;
+                do
+                {
+                    Random random = new Random();
+                    Console.WriteLine("Digite o número de mensagens para enviar:");
+                    int vezes = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < vezes; i++)
+                    {
+                        Publish(connection, random.Next(1, 10000).ToString());
+                    }
+                    Console.WriteLine("Continuar? (1 - Sim / 0 - Não)");
+                    opcao = int.Parse(Console.ReadLine());
+                } while (opcao != 0);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static void Publish(IConnection connection, string message)
+        {
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "demo_rabbitmq",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "demo_rabbitmq",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+
+            }
+        }
+    }
+}
